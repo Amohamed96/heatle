528cc9a40eed8767e7ef221446bdc2cc757ec207
Amohamed96 <amohamed.rg@gmail.com>
2022-04-02 13:30:34 +0200

    Letters, right or wrong

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import Keyboard from "./components/Keyboard";
 import Board from "./components/Board";
+import { createContext, useState } from "react";
+import { boardDefault } from "./Words";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Picto</h1>
       </nav>
-      <Board />
-      <Keyboard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
-import { boardDefault } from "../Words";
 import Letter from "./Letter";
 
 function Board() {
-  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="board">
-      {""}
       <div className="row">
         <Letter letterPos={0} attemptVal={0} />
         <Letter letterPos={1} attemptVal={0} />
