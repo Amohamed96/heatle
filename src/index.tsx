6f42ee64f3fefc88bbf9a7f3a197b788237041ce
Amohamed96 <amohamed.rg@gmail.com>
2022-05-01 07:40:07 +0200

    Replace BrowserRouter with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { AlertProvider } from './context/AlertContext'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <AlertProvider>
       <App />
     </AlertProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 
   document.getElementById('root')
 )
